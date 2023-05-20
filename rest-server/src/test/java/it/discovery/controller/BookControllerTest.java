5949179cbc70c16fb0519e18058812870ed2793e
Sergiy <sergey.morenets@gmail.com>
2023-05-20 18:11:15 +0300

    Task 9

--- rest-server/src/test/java/it/discovery/controller/BookControllerTest.java
+++ rest-server/src/test/java/it/discovery/controller/BookControllerTest.java
@@ -6,19 +6,24 @@ import it.discovery.dto.BookDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = RestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class BookControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -40,8 +45,16 @@ public class BookControllerTest {
         bookDTO.setAuthor("Jones");
         bookDTO.setYear(2022);
 
-        mockMvc.perform(post("/api/books").contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(MAPPER.writeValueAsBytes(bookDTO))).andExpect(status().isBadRequest());
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(MAPPER.writeValueAsBytes(bookDTO))).andExpect(status().isBadRequest())
+                .andDo(document("{class-name}/{method-name}",
+                        requestFields(fieldWithPath("id").description("Book identifier"),
+                                fieldWithPath("title").description("Book title"),
+                                fieldWithPath("author").description("Book author"),
+                                fieldWithPath("year").description("Publishing year"),
+                                fieldWithPath("orders").description("Book orders"),
+                                fieldWithPath("amount").description("Number of the books in the store"))));
     }
 
 }
