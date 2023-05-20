3d9601f1c944bc312ee821e30dc67a4528b56d71
Sergiy <sergey.morenets@gmail.com>
2023-05-20 17:32:14 +0300

    Task 8

---
+++ rest-server/src/main/java/it/discovery/cache/CacheConfiguration.java
@@ -0,0 +1,33 @@
+package it.discovery.cache;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.Kryo5Codec;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CacheConfiguration {
+
+    @Bean(destroyMethod = "shutdown")
+    RedissonClient redissonClient() {
+        Config config = new Config();
+        config.setCodec(new Kryo5Codec());
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+        return Redisson.create(config);
+    }
+
+    @Bean
+    CacheManager cacheManager(RedissonClient redissonClient) {
+        return new RedissonSpringCacheManager(redissonClient, new HashMap<>(Map.of("books",
+                new CacheConfig(60_000, 60_000))));
+    }
+}
