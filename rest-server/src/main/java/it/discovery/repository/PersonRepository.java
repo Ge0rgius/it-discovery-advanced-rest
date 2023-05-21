d03f684da5f6cacd0ead0d6b491cb4cd38338a74
Sergiy <sergey.morenets@gmail.com>
2023-05-21 16:42:51 +0300

    Task 15. Fixed dependency

--- rest-server/src/main/java/it/discovery/repository/PersonRepository.java
+++ rest-server/src/main/java/it/discovery/repository/PersonRepository.java
@@ -2,6 +2,11 @@ package it.discovery.repository;
 
 import it.discovery.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
 }
